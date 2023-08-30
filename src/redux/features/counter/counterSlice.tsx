bd1187f91236d8a3e3677dc33c4551f7078ec3e9
masudrana-1 <masudrana58855@gmail.com>
2023-08-30 10:48:28 +0600

    improve typescript and add hooks

--- src/redux/features/counter/counterSlice.tsx
+++ src/redux/features/counter/counterSlice.tsx
@@ -1,6 +1,10 @@
 import {PayloadAction, createSlice} from "@reduxjs/toolkit"
 
-const initialState = {
+interface CounterState {
+    count : number
+}
+
+const initialState : CounterState = {
     count: 0
 }
 
