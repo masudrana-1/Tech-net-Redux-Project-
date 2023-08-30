2a88cb02886767743e3e2dc7b2c22218d9317296
masudrana-1 <masudrana58855@gmail.com>
2023-08-30 10:18:48 +0600

    added useDispatch and use increment and decrement

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
 import { RootState } from "./redux/store"
+import { decrement, increment } from "./redux/features/counter/counterSlice";
 
 function App() {
 
-  const { count } = useSelector((state: RootState) => state.counter)
-  // const dispatch = useDispatch()
+  const { count } = useSelector((state: RootState) => state.counter);
+  const dispatch = useDispatch();
 
 
 
   return (
     <div>
       <div className="">
-          <button className="bg-rose-300 p-6 rounded-lg gap-6">Increment</button>
+          <button className="bg-rose-300 p-6 rounded-lg gap-6" onClick={()=> dispatch(increment())}>Increment</button>
           <div>
             {count}
           </div> 
-          <button className="bg-rose-300 p-6 rounded-lg">Decrement</button>
+          <button className="bg-rose-300 p-6 rounded-lg" onClick={()=> dispatch(decrement())}>Decrement</button>
       </div>
     </div>
   )
--- src/redux/features/counter/counterSlice.tsx
+++ src/redux/features/counter/counterSlice.tsx
@@ -19,4 +19,6 @@ const counterSlice = createSlice({
 })
 
 
+export const {increment, decrement} = counterSlice.actions;
+
 export default counterSlice.reducer;
