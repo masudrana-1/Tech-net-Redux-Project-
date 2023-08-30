59c6fd06e56f616392ca331e141d26b52576cba0
masudrana-1 <masudrana58855@gmail.com>
2023-08-30 11:24:12 +0600

    add logger and custom logger

---
+++ src/redux/middleware/logger.tsx
@@ -0,0 +1,15 @@
+// custom middleware 
+
+import { Middleware } from "@reduxjs/toolkit"
+
+
+
+const logger: Middleware = (store)=> (next)=> (action)=>{
+    console.log('Current State', store.getState());
+    console.log('Action', action);
+
+    next(action);
+    console.log('Updated State', store.getState());
+}
+
+export default logger;
--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/counter/counterSlice"
+import logger from "./middleware/logger";
+// import logger from "redux-logger";
 
 const store = configureStore({
     reducer: {
         counter : counterReducer,
     },
+
+    // middleware
+    // middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+
+    // custom middleware 
+    middleware : (getDefaultMiddleware) => getDefaultMiddleware().concat(logger)
 })
 
 
