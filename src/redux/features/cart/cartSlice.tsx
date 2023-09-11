45e76107eaddc4445de895850caa1adee64eea66
masudrana-1 <masudrana58855@gmail.com>
2023-09-11 10:12:01 +0600

    remove one from cart and full remove

--- src/redux/features/cart/cartSlice.tsx
+++ src/redux/features/cart/cartSlice.tsx
@@ -36,11 +36,12 @@ const cartSlice = createSlice({
                 (product) => product._id === action.payload._id
             );
 
-            if(existing){
+            if(existing && existing.quantity! > 1){
                 existing.quantity = existing.quantity! - 1;
             }else{
-                
-                state.products.push({...action.payload, quantity: 1});
+                state.products = state.products.filter(
+                    (product) => product._id !== action.payload._id
+                )
             }
         },
 
