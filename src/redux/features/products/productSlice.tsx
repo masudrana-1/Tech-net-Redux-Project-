e9d1355960da213d2c6f5a288d9e7e70054db7c4
masudrana-1 <masudrana58855@gmail.com>
2023-09-13 11:28:13 +0600

    added filter

--- src/redux/features/products/productSlice.tsx
+++ src/redux/features/products/productSlice.tsx
@@ -1,6 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-// import type { PayloadAction } from "@reduxjs/toolkit";
-
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface IProducts {
     status: boolean;
@@ -17,8 +16,15 @@ const productSlice = createSlice({
     name: 'product',
     initialState,
     reducers: {
-
+        toggleState: (state) =>{
+            state.status = !state.status;
+        },
+        setPriceRange: (state, action: PayloadAction<number>) =>{
+            state.priceRange = action.payload;
+        }
     }
 });
+
+export const {toggleState, setPriceRange} = productSlice.actions;
 
 export default productSlice.reducer;
