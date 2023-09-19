ecb2346796c2b8db4d5f276a62e8fa20a43266c0
masudrana-1 <masudrana58855@gmail.com>
2023-09-19 10:29:28 +0600

    connect api get to store

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,12 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import cartReducer from './features/cart/cartSlice';
 import productReducer from './features/products/productSlice';
+import { api } from './api/apiSlice';
 
 
 const store = configureStore({
     reducer: {
         cart: cartReducer,
         product: productReducer,
+        [api.reducerPath] : api.reducer,
     },
 });
 
