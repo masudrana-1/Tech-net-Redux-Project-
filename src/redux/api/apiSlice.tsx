a090e83a8bb598507880efc3e83379f2b947b553
masudrana-1 <masudrana58855@gmail.com>
2023-09-20 12:13:54 +0600

    post method add and postComment api create in apiSlice

--- src/redux/api/apiSlice.tsx
+++ src/redux/api/apiSlice.tsx
@@ -1,18 +1,31 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl : 'http://localhost:5000'}),
     endpoints: (builder) => ({
 
-        //! get route 
+        //! get method
         getProducts : builder.query({
             query: ()=> "/products"
         }),
         getSingleProducts : builder.query({
             query: (id)=> `/product/${id}`
         }),
+
+        //! post method 
+        postComment: builder.mutation({
+            query: ({id, data})=> ({
+                url: `/comment/${id}`,
+                method: "POST",
+                body: data,
+            }),
+        }),
     }),
 });
 
-export const {useGetProductsQuery, useGetSingleProductsQuery} = api;
+export const {
+    useGetProductsQuery, 
+    useGetSingleProductsQuery, 
+    usePostCommentMutation,
+} = api;
