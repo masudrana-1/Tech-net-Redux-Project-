792c559db5cd190ab040b26a09a698c4f02e9ce0
masudrana-1 <masudrana58855@gmail.com>
2023-09-23 10:21:13 +0600

    remove endpoint code from apiSlice

--- src/redux/api/apiSlice.tsx
+++ src/redux/api/apiSlice.tsx
@@ -4,37 +4,39 @@ export const api = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({baseUrl : 'http://localhost:5000'}),
     tagTypes: ['comments'],
-    endpoints: (builder) => ({
+    endpoints: () => ({
 
-        //! get method
-        getProducts : builder.query({
-            query: ()=> "/products"
-        }),
-        getSingleProducts : builder.query({
-            query: (id)=> `/product/${id}`
-        }),
 
-        //! post method 
-        postComment: builder.mutation({
-            query: ({id, data})=> ({
-                url: `/comment/${id}`,
-                method: "POST",
-                body: data,
-            }),
-            invalidatesTags: ['comments'],
-        }),
+        //! ekhan theke sob endpoint productApi file a splitting kora hoyeche
+    //     //! get method
+    //     getProducts : builder.query({
+    //         query: ()=> "/products"
+    //     }),
+    //     getSingleProducts : builder.query({
+    //         query: (id)=> `/product/${id}`
+    //     }),
 
-        //! get method
-        getComment : builder.query({
-            query: (id)=> `/comment/${id}`,
-            providesTags: ['comments']
-        }),
+    //     //! post method 
+    //     postComment: builder.mutation({
+    //         query: ({id, data})=> ({
+    //             url: `/comment/${id}`,
+    //             method: "POST",
+    //             body: data,
+    //         }),
+    //         invalidatesTags: ['comments'],
+    //     }),
+
+    //     //! get method
+    //     getComment : builder.query({
+    //         query: (id)=> `/comment/${id}`,
+    //         providesTags: ['comments']
+    //     }),
     }),
 });
 
-export const {
-    useGetProductsQuery, 
-    useGetSingleProductsQuery, 
-    usePostCommentMutation,
-    useGetCommentQuery,
-} = api;
+// export const {
+//     useGetProductsQuery, 
+//     useGetSingleProductsQuery, 
+//     usePostCommentMutation,
+//     useGetCommentQuery,
+// } = api;
