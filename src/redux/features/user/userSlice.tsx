04f2667b3af8caf42a8b1f0ac303c5df5f506263
masudrana-1 <masudrana58855@gmail.com>
2023-09-25 20:11:48 +0600

    case write

--- src/redux/features/user/userSlice.tsx
+++ src/redux/features/user/userSlice.tsx
@@ -43,13 +43,17 @@ const userSlice = createSlice({
     reducers: {},
     extraReducers: (builder)=> {
         builder.addCase(createUser.pending, (state)=>{
-            state.isLoading = true
-        }).addCase(createUser.fulfilled, ()=>{
-
-        }).addCase(createUser.rejected, ()=>{
-            
+            state.isLoading = true;
+            state.isError = false;
+            state.error = null;
+        }).addCase(createUser.fulfilled, (state, action)=>{
+            state.user.email = action.payload;
+        }).addCase(createUser.rejected, (state, action)=>{
+            state.user.email = null;
+            state.isError = true;
+            state.error = action.error.message!;
         })
-    }
+    },
 });
 
 export default userSlice.reducer;
