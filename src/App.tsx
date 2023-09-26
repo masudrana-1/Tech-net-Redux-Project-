d26ec98798b4189ba31fcba337f18a8c5b938807
masudrana-1 <masudrana58855@gmail.com>
2023-09-26 10:53:53 +0600

    setUse in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
+import { onAuthStateChanged } from 'firebase/auth';
 import { Toaster } from './components/ui/Toaster';
 import MainLayout from './layouts/MainLayout';
+import { useAppDispatch } from './redux/hook';
+import { auth } from './lib/firebase.config';
+import { setUser } from './redux/features/user/userSlice';
 
 function App() {
 
+  //! Redux for setUser
+  const dispatch = useAppDispatch();
   
+  //! firebase and redux for setUser
+  onAuthStateChanged(auth, (user)=>{
+    if(user){
+      dispatch(setUser(user.email));
+    }
+  }) 
 
 
   return (
