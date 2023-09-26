389d260169711515d876c8d2af2166e719370734
masudrana-1 <masudrana58855@gmail.com>
2023-09-26 10:19:21 +0600

    create loginUser case

--- src/redux/features/user/userSlice.tsx
+++ src/redux/features/user/userSlice.tsx
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { auth } from '@/lib/firebase.config'
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 interface IUserState {
     user: {
@@ -25,11 +25,21 @@ const initialState: IUserState = {
     error : null,
 };
 
-
+//! signup 
 export const createUser = createAsyncThunk(
     'user/createUser',
     async ({email, password}: ICredential )=>{
         const data = await createUserWithEmailAndPassword(auth, email, password);
+
+        return data.user.email;
+    }
+);
+
+//! login 
+export const loginUser = createAsyncThunk(
+    'user/loginUser',
+    async ({email, password}: ICredential )=>{
+        const data = await signInWithEmailAndPassword(auth, email, password);
 
         return data.user.email;
     }
@@ -41,6 +51,8 @@ const userSlice = createSlice({
     initialState,
     reducers: {},
     extraReducers: (builder)=> {
+
+        //! signup user case 
         builder.addCase(createUser.pending, (state)=>{
             state.isLoading = true;
             state.isError = false;
@@ -49,6 +61,20 @@ const userSlice = createSlice({
             state.user.email = action.payload;
             state.isLoading = false;
         }).addCase(createUser.rejected, (state, action)=>{
+            state.user.email = null;
+            state.isError = true;
+            state.error = action.error.message!;
+        })
+
+        //! login user case 
+        builder.addCase(loginUser.pending, (state)=>{
+            state.isLoading = true;
+            state.isError = false;
+            state.error = null;
+        }).addCase(loginUser.fulfilled, (state, action)=>{
+            state.user.email = action.payload;
+            state.isLoading = false;
+        }).addCase(loginUser.rejected, (state, action)=>{
             state.user.email = null;
             state.isError = true;
             state.error = action.error.message!;
