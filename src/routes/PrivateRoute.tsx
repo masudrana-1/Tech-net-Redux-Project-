b59dba5e060eac640b3dc9c3c6b43abe0c95e367
masudrana-1 <masudrana58855@gmail.com>
2023-09-27 11:10:21 +0600

    add private route and wrap private route in checkout

---
+++ src/routes/PrivateRoute.tsx
@@ -0,0 +1,26 @@
+import { useAppSelector } from "@/redux/hook";
+import { ReactNode } from "react";
+import { Navigate, useLocation } from "react-router-dom";
+
+interface IProps {
+    children: ReactNode;
+}
+
+export default function PrivateRoute({children}: IProps) {
+
+    const {user, isLoading} = useAppSelector(state=> state.user);
+
+    const {pathname} = useLocation();
+
+    if(isLoading){
+        return <p>Loading.........wait.........👀🫵</p>;
+    }
+
+    if(!user.email && !isLoading){
+        return <Navigate to="/login" state={{path: pathname}} />
+    }
+
+
+
+  return children;
+}
